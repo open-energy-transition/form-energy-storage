926f763d7c05c1ef8b294d338848f6a5617d9fc2
Daniel Rüdt <117752024+daniel-rdt@users.noreply.github.com>
2024-11-14 16:02:19 +0100

    overwrite cost table attributes specified in config in prepare_costs and load_costs (#23)
    * overwrite cost table attributes specified in config in prepare_costs and load_costs
    
    * add custom fuel prices from EEE project
    
    ---------
    
    Co-authored-by: Thomas Gilon <thomas.gilon@openenergytransition.org>

--- scripts/prepare_sector_network.py
+++ scripts/prepare_sector_network.py
@@ -1090,6 +1090,15 @@ def prepare_costs(cost_file, params, nyears):
 
     costs = costs.fillna(params["costs"]["fill_values"])
 
+    for attr in ("investment", "lifetime", "FOM", "VOM", "efficiency", "fuel"):
+        overwrites = params["costs"].get(attr)
+        if overwrites is not None:
+            overwrites = pd.Series(overwrites)
+            costs.loc[overwrites.index, attr] = overwrites
+            logger.info(
+                f"Overwriting {attr} of {overwrites.index} to {overwrites.values}"
+            )
+
     def annuity_factor(v):
         return calculate_annuity(v["lifetime"], v["discount rate"]) + v["FOM"] / 100
 
