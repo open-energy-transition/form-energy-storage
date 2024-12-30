Fix scaling issues due to snapshot weightings, add options to use ENTSO-E load data in electricity demand preparations, missing cost years are replaced with the closest available year

scripts/plot_validation_cross_border_flows.py
@@ -7,6 +7,7 @@
 import country_converter as coco
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 import pypsa
 import seaborn as sns
 from _helpers import configure_logging, set_scenario_config
@@ -113,13 +114,13 @@ def cross_border_time_series(countries, data):
                 ymin = neg_min
 
             pos_max = df_pos.sum(axis=1).max() * 1.2
-            if pos_max < ymax:
+            if pos_max > ymax:
                 ymax = pos_max
 
             axis = axis + 1
 
         for x in range(axis - 2, axis):
-            ax[x].set_ylim([neg_min, pos_max])
+            ax[x].set_ylim([ymin, ymax])
 
     fig.savefig(snakemake.output.trade_time_series, bbox_inches="tight")
     plt.close(fig)
@@ -206,7 +207,22 @@ if __name__ == "__main__":
     )
 
     if len(historic.index) > len(n.snapshots):
-        historic = historic.resample(n.snapshots.inferred_freq).mean().loc[n.snapshots]
+        if n.snapshots.inferred_freq:
+            historic = historic.resample(n.snapshots.inferred_freq).sum().loc[n.snapshots]
+        else:
+            # if frequency is inconsistent due to segmentation
+            snapshots_interval = pd.Series({n.snapshots[x]:(n.snapshots[x+1] - n.snapshots[x])/np.timedelta64(1,'h') for x in range(0,len(n.snapshots)-1)})
+            snapshots_interval_last = pd.Series({n.snapshots[-1]:(pd.Timestamp(snakemake.config["snapshots"]["end"]) - n.snapshots[-1])/np.timedelta64(1,'h')})
+            snapshots_interval = pd.concat([snapshots_interval, snapshots_interval_last])
+
+            first_bin = [n.snapshots[0]]
+            mid_bins = [x + pd.DateOffset(hours=snapshots_interval[x]/2) for x in n.snapshots[:-1]]
+            last_bin = [n.snapshots[-1] + pd.DateOffset(hours=snapshots_interval[-1])]
+            bins = pd.DatetimeIndex(first_bin + mid_bins + last_bin)
+
+            interval_labels = pd.cut(historic.index, bins=bins, right=False)
+            historic = historic.groupby(interval_labels).sum()
+            historic.index = pd.to_datetime(n.snapshots)
 
     # Set the historic date based on the snapshot year
     if historic.index.year.unique()[0] != n.snapshots.year.unique()[0]:
@@ -237,8 +253,12 @@ if __name__ == "__main__":
                 optimized = optimized.rename(columns={c1: c2})
 
     optimized = optimized.groupby(lambda x: x, axis=1).sum()
+    optimized = optimized.mul(n.snapshot_weightings.generators, axis=0)
 
     cross_border_bar(countries, [historic, optimized])
+
+    optimized = optimized.div(n.snapshot_weightings.generators, axis=0)
+    historic = historic.div(n.snapshot_weightings.generators, axis=0)
 
     cross_border_time_series(countries, [historic, optimized])
 

scripts/plot_validation_electricity_prices.py
@@ -6,6 +6,7 @@
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 import pypsa
 import seaborn as sns
 from _helpers import configure_logging, set_scenario_config
@@ -36,7 +37,23 @@ if __name__ == "__main__":
     )
 
     if len(historic.index) > len(n.snapshots):
-        historic = historic.resample(n.snapshots.inferred_freq).mean().loc[n.snapshots]
+        if n.snapshots.inferred_freq:
+            historic = historic.resample(n.snapshots.inferred_freq).mean().loc[n.snapshots]
+        else:
+            # if frequency is inconsistent due to segmentation
+            snapshots_interval = pd.Series({n.snapshots[x]:(n.snapshots[x+1] - n.snapshots[x])/np.timedelta64(1,'h') for x in range(0,len(n.snapshots)-1)})
+            snapshots_interval_last = pd.Series({n.snapshots[-1]:(pd.Timestamp(snakemake.config["snapshots"]["end"]) - n.snapshots[-1])/np.timedelta64(1,'h')})
+            snapshots_interval = pd.concat([snapshots_interval, snapshots_interval_last])
+
+            first_bin = [n.snapshots[0]]
+            mid_bins = [x + pd.DateOffset(hours=snapshots_interval[x]/2) for x in n.snapshots[:-1]]
+            last_bin = [n.snapshots[-1] + pd.DateOffset(hours=snapshots_interval[-1])]
+            bins = pd.DatetimeIndex(first_bin + mid_bins + last_bin)
+
+            interval_labels = pd.cut(historic.index, bins=bins, right=False)
+            historic = historic.groupby(interval_labels).sum()
+            historic.index = pd.to_datetime(n.snapshots)
+            historic = historic.div(n.snapshot_weightings.generators, axis=0)
 
     optimized = n.buses_t.marginal_price.T.groupby(n.buses.country).mean().T
 

scripts/plot_validation_electricity_production.py
@@ -134,7 +134,22 @@ if __name__ == "__main__":
     colors["VRES"] = "#0fa101"
 
     if len(historic.index) > len(n.snapshots):
-        historic = historic.resample(n.snapshots.inferred_freq).mean().loc[n.snapshots]
+        if n.snapshots.inferred_freq:
+            historic = historic.resample(n.snapshots.inferred_freq).sum().loc[n.snapshots]
+        else:
+            # if frequency is inconsistent due to segmentation
+            snapshots_interval = pd.Series({n.snapshots[x]:(n.snapshots[x+1] - n.snapshots[x])/np.timedelta64(1,'h') for x in range(0,len(n.snapshots)-1)})
+            snapshots_interval_last = pd.Series({n.snapshots[-1]:(pd.Timestamp(snakemake.config["snapshots"]["end"]) - n.snapshots[-1])/np.timedelta64(1,'h')})
+            snapshots_interval = pd.concat([snapshots_interval, snapshots_interval_last])
+
+            first_bin = [n.snapshots[0]]
+            mid_bins = [x + pd.DateOffset(hours=snapshots_interval[x]/2) for x in n.snapshots[:-1]]
+            last_bin = [n.snapshots[-1] + pd.DateOffset(hours=snapshots_interval[-1])]
+            bins = pd.DatetimeIndex(first_bin + mid_bins + last_bin)
+
+            interval_labels = pd.cut(historic.index, bins=bins, right=False)
+            historic = historic.groupby(interval_labels).sum()
+            historic.index = pd.to_datetime(n.snapshots)
 
     # Set the historic date based on the snapshot year
     historic_year = historic.index.year.unique()[0]
@@ -154,10 +169,10 @@ if __name__ == "__main__":
 
     # Compare carrier where historical data are available
     optimized = optimized.loc[:, optimized.columns.get_level_values(1).isin(historic.columns.get_level_values(1))]
+    optimized = optimized.mul(n.snapshot_weightings.generators, axis=0)
 
     data = pd.concat([historic, optimized], keys=["Historic", "Optimized"], axis=1)
     data.columns.names = ["Kind", "Country", "Carrier"]
-    data = data.mul(n.snapshot_weightings.generators, axis=0)
 
     # revert back datetime according to historical year
     data.index = data.index.map(lambda x: x.replace(year=historic_year))
@@ -222,9 +237,10 @@ if __name__ == "__main__":
     fig, axes = plt.subplots(3, 1, figsize=(9, 9))
 
     df = (
-        data.groupby(level=["Kind", "Carrier"], axis=1)
+        data.div(n.snapshot_weightings.generators, axis=0)
+        .groupby(level=["Kind", "Carrier"], axis=1)
         .sum()
-        .resample("1W")
+        .resample("1D")
         .mean()
         .clip(lower=0)
     )
@@ -246,9 +262,12 @@ if __name__ == "__main__":
     diff.clip(lower=0).plot.area(
         ax=axes[2], **kwargs, title="$\Delta$ (Optimized - Historic)"
     )
-    lim = axes[1].get_ylim()[1]/2
     diff.clip(upper=0).plot.area(ax=axes[2], **kwargs)
-    axes[2].set_ylim(bottom=-lim, top=lim)
+
+    ylim = max(axes[0].get_ylim()[1],axes[1].get_ylim()[1])
+    axes[0].set_ylim(top=ylim)
+    axes[1].set_ylim(top=ylim)
+    axes[2].set_ylim(bottom=-ylim/2, top=ylim/2)
 
     h, l = axes[0].get_legend_handles_labels()
     fig.legend(