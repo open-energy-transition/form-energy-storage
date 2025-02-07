b7475ec48837e64b74a0aec79ee9853c7c15baa9
daniel-rdt <daniel.ruedt@openenergytransition.org>
2025-02-07 11:32:58 +0100

    add filter_year for adding transmission projects

--- scripts/add_transmission_projects_and_dlr.py
+++ scripts/add_transmission_projects_and_dlr.py
@@ -24,13 +24,16 @@ def attach_transmission_projects(
     for path in transmission_projects:
         path = Path(path)
         df = pd.read_csv(path, index_col=0, dtype={"bus0": str, "bus1": str})
+        filter_year = params.get("transmission_projects").get("filter_year", np.inf)
         if df.empty:
             continue
         if "new_buses" in path.name:
             n.add("Bus", df.index, **df)
         elif "new_lines" in path.name:
+            df.query("build_year <= @filter_year", inplace=True)
             n.add("Line", df.index, **df)
         elif "new_links" in path.name:
+            df.query("build_year <= @filter_year", inplace=True)
             n.add("Link", df.index, **df)
         elif "adjust_lines" in path.name:
             n.lines.update(df)
@@ -72,7 +75,13 @@ if __name__ == "__main__":
     if "snakemake" not in globals():
         from _helpers import mock_snakemake
 
-        snakemake = mock_snakemake("add_transmission_projects_and_dlr")
+        snakemake = mock_snakemake("add_transmission_projects_and_dlr",
+                                   opts="",
+                                   clusters="52",
+                                   ll="v1.11",
+                                   sector_opts="",
+                                   run="baseline-neplinesandlinks"
+                                   )
     configure_logging(snakemake)
     set_scenario_config(snakemake)
 
